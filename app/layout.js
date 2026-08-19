dcd773f52d49f36e6c7f55d4e27bd0567acad5b7
RavinduTharanga <ravindutharangaperera@gmail.com>
2026-08-19 19:59:09 -0400

    Add Home Page

--- app/layout.js
+++ app/layout.js
@@ -40,7 +40,7 @@ export default function RootLayout({ children }) {
           />
         )}
       </head>
-      <body style={{ margin: 0, fontFamily: "system-ui, sans-serif", background: "#b3b9c4", color: "#fafafa" }}>
+      <body style={{ margin: 0, fontFamily: "system-ui, sans-serif", background: "#f2f3f5", color: "#fafafa" }}>
         <Providers>
           <Navbar />
           {children}
