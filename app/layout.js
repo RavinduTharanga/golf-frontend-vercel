69dafc9e08015f9d56908e9da7908c1b71781056
RavinduTharanga <ravindutharangaperera@gmail.com>
2026-08-19 22:12:06 -0400

    Add Subcriptions

--- app/layout.js
+++ app/layout.js
@@ -40,7 +40,7 @@ export default function RootLayout({ children }) {
           />
         )}
       </head>
-      <body style={{ margin: 0, fontFamily: "system-ui, sans-serif", background: "#f2f3f5", color: "#fafafa" }}>
+      <body style={{ margin: 0, fontFamily: "system-ui, sans-serif", background: "#0e1117", color: "#fafafa" }}>
         <Providers>
           <Navbar />
           {children}
